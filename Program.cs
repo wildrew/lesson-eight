0fc4248d247a26d05294ec84409f1d677c2aa06b
wildrew <drewco@cox.net>
2017-06-05 22:20:01 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Main method demonstrates the use of variables, operators, expressions, and statements
+            int x = 3;
+            int y = 4;
+            int sum = x + y;
+            Console.WriteLine("Sum of " + x + " and " + y + " is: " + sum);
+            Console.WriteLine("Are the values sum and 7 equal? " + (sum == 7));
+            string text1 = "Hello";
+            string text2 = "World";
+            string message = text1 + " " + text2;
+            Console.WriteLine("Message: " + message);
+        }
+    }
+}
